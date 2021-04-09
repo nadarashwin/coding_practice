161fab868c461ff4eb160c35f6aa9b3b3e963002
Ashwin Balakrishnan <nadarashwin@gmail.com>
2021-04-09 20:04:26 +0530

    sort dict by value - orderedDict

--- dict_sort_by_value.py
+++ dict_sort_by_value.py
@@ -1,1 +1,14 @@
-# Given a dictionary sort the dict 
+# Given a dictionary sort the dict by value and for similar values, sort it by key
+
+def sort_dict(data):
+    # OrderedDict preserves the order in which the keys are inserte
+    from collections import OrderedDict
+    data = OrderedDict(sorted(data.items()))
+
+    return sorted(data.items(), key=lambda x: x[-1], reverse=True)
+
+
+
+data = {'abc':30, 'bcd': 10, 'jkl':40, 'cde': 40, 'def': 35, 'efg': 10, 'fgh': 50, 'ghi': 40, 'hij':23}
+
+print(sort_dict(data))
