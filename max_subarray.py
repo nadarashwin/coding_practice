d78fe8982726986eb53890c1c9738c0b8e11f4df
Ashwin Balakrishnan <nadarashwin@gmail.com>
2021-04-16 12:47:09 +0530

    buy sell stocks

--- max_subarray.py
+++ max_subarray.py
@@ -1,4 +1,5 @@
-# Given an array arr of N integers. Find the contiguous sub-array with maximum sum. (Kadane's Algorithm)
+# Given an array arr of N integers.
+# Find the contiguous sub-array with maximum sum. (Kadane's Algorithm)
 
 # Example 1:
 
@@ -9,23 +10,29 @@
 # 9
 # Explanation:
 # Max subarray sum is 9
-# of elements (1, 2, 3, -2, 5) which 
+# of elements (1, 2, 3, -2, 5) which
 # is a contiguous subarray.
 
 def max_subarray(data):
 
-    ## Block of code to tackle the issue where all the elments are negative.
+    # Block of code to tackle the issue where all the elments are negative.
     maximum = max(data)
+    # if the list contains all negative values, return the maximum element
     if maximum < 0:
         return maximum
 
-    maxi = current = 0
+    # stores the maximum sum sublist found so far
+    maxi = 0
+
+    # stores the maximum sum of sublist ending at the current position
+    current = 0
+
     for element in data:
         current = current + element
-        current = max(current, 0) # if current is negative, set it to 0
-        maxi = max(current, maxi) # update maxi if the current sublist is greater
+        current = max(current, 0)  # if current is negative, set it to 0
+        maxi = max(current, maxi)  # update maxi if current sublist is greater
+        print(current, maxi)
     return maxi
-
 
 
 a = [-2, -3, 4, -1, -2, 1, 5, -3]
